88dfc5e4df69b6d6840e7996ef6acd05d96ad832
mwakisaghu <harrisonchambela2016@gmail.com>
2021-12-01 12:56:52 +0300

    API Caching

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,18 @@ const url = require('url');
 const express = require('express');
 const router = express.Router();
 const needle = require('needle');
+const apicache = require('apicache');
 
 //Environment Variables
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
+//Initialized cache
+let cache = apicache.middleware;
+
 //Create Routes
-router.get('/', async (req, res, next) => {
+router.get('/', cache('2 minutes'), async (req, res) => {
   try {
     const params = new URLSearchParams({
       [API_KEY_NAME]: API_KEY_VALUE,
